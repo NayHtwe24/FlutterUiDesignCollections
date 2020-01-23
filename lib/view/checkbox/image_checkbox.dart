27c607709f3ad6877a0348b3f67013635bc1dc28
NayHtwe24 <43719216+NayHtwe24@users.noreply.github.com>
2020-01-23 13:46:09 +0630

    Checkbox Complete

--- lib/view/checkbox/image_checkbox.dart
+++ lib/view/checkbox/image_checkbox.dart
@@ -19,7 +19,163 @@ class _ImageCheckBoxState extends State<ImageCheckBox> {
         title: Text("Image CheckBox"),
       ),
       body: Container(
-
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 30.0),
+              ),
+              Text("Hobby"),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 30),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/dance.png"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Expanded(
+                          child: Text(
+                            "Dance",
+                          )),
+                      Checkbox(
+                        value: DanceVal,
+                        activeColor: Colors.red,
+                        onChanged: (val) {
+                          setState(() {
+                            DanceVal = val;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 30),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/sing.png"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Expanded(
+                          child: Text(
+                            "Singing",
+                          )),
+                      Checkbox(
+                        value: DanceVal,
+                        activeColor: Colors.red,
+                        onChanged: (val) {
+                          setState(() {
+                            DanceVal = val;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 30),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/reading.png"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Expanded(
+                          child: Text(
+                            "Reading",
+                          )),
+                      Checkbox(
+                        value: DanceVal,
+                        activeColor: Colors.red,
+                        onChanged: (val) {
+                          setState(() {
+                            DanceVal = val;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 30),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/study.png"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                      ),
+                      Expanded(
+                          child: Text(
+                            "Studying",
+                          )),
+                      Checkbox(
+                        value: DanceVal,
+                        activeColor: Colors.red,
+                        onChanged: (val) {
+                          setState(() {
+                            DanceVal = val;
+                          });
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
       ),
     );
   }
--- lib/view/checkbox/list_checkbox.dart
+++ lib/view/checkbox/list_checkbox.dart
@@ -5,12 +5,86 @@ class ListCheckBox extends StatefulWidget {
 }
 
 class _ListCheckBoxState extends State<ListCheckBox> {
+
+  List<String> inputs = List<String>();
+  List<bool> check = List<bool>();
+
   @override
+
+  void initState() {
+    setState(() {
+      //value of CheckBoxs
+      check.add(true);
+      check.add(false);
+      check.add(true);
+      check.add(false);
+      check.add(false);
+      check.add(false);
+      check.add(false);
+      check.add(true);
+
+      // Items of CheckBoxs List
+      inputs.add("Movie");
+      inputs.add("Food");
+      inputs.add("Song");
+      inputs.add("Dance");
+      inputs.add("Actor");
+      inputs.add("Artist");
+      inputs.add("Travelling");
+      inputs.add("Study");
+    });
+  }
+
+  // calling function when item Slect or Deselect
+  void ItemChange(bool val, int index, BuildContext context) {
+    setState(() {
+      check[index] = val;
+    });
+    if (check[index] == true) {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.black,
+          content: Text("Select ${inputs[index]}"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+    } else {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.black,
+          content: Text("DeSelect ${inputs[index]}"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("List Check Box"),
       ),
+      body: ListView.builder(
+          itemCount: inputs.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    CheckboxListTile(
+                        value: check[index],
+                        activeColor: Colors.red,
+                        title: Text('${inputs[index]}'),
+                        controlAffinity: ListTileControlAffinity.leading,
+                        onChanged: (bool val) {
+                          ItemChange(val, index, context);
+                        })
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
